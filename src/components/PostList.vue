b3c00a7dde260fde04054e6c187e17c69f2cb281
murata <murata.small.baseball@gmail.com>
2021-07-23 20:38:34 +0900

    テーブルからカードに変更

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="post-list">
-    <v-simple-table class="post-list">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th>タイトル</th>
-            <th>内容</th>
-            <th>投稿日時</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="post in posts" :key="post.name">
-            <th>{{post.fields.name.stringValue}}</th>
-            <th>{{post.fields.comment.stringValue}}</th>
-            <th>{{post.fields.created.timestampValue}}</th>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <v-card
+    class="post-contents"
+    max-width="400"
+    outlined
+    v-for="post in posts" :key="post.name"
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          {{post.fields.name.stringValue}}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{post.fields.comment.stringValue}}</v-list-item-subtitle>
+      </v-list-item-content>
+      <v-card-actions>
+        {{post.fields.created.timestampValue}}
+      </v-card-actions>
+    </v-list-item>
+  </v-card>
   </div>
 </template>
 
@@ -32,3 +32,10 @@ export default {
   }
 }
 </script>
+
+<style>
+.post-contents {
+  @apply mx-auto;
+  @apply mb-5;
+}
+</style>
