0c7c9ecbe6ae348a0f6cf69cf90a34f9c8ba3b4b
murata <murata.small.baseball@gmail.com>
2021-07-23 20:23:33 +0900

    post-list作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,21 @@
 <template>
   <div id="home">
-    <button @click="addComment">テスト</button>
-    <v-simple-table class="post-list">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th>タイトル</th>
-            <th>内容</th>
-            <th>投稿日時</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="post in posts" :key="post.name">
-            <th>{{post.fields.name.stringValue}}</th>
-            <th>{{post.fields.comment.stringValue}}</th>
-            <th>{{post.fields.created.timestampValue}}</th>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <post-list :posts="posts"></post-list>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import PostList from '../components/PostList.vue'
 
 const URL = "https://firestore.googleapis.com/v1/projects/" +
     process.env.VUE_APP_FIREBASE_DB + "/databases/(default)/documents/comments"
 
 export default {
   name: 'home',
+  components: {
+    PostList,
+  },
   data: () => ({
     posts: [],
   }),
