de7352919476fc074d7e0e1f59ff5faa66c22021
murata <murata.small.baseball@gmail.com>
2021-07-27 21:29:47 +0900

    slotを追加

--- src/components/HeaderText.vue
+++ src/components/HeaderText.vue
@@ -1,6 +1,7 @@
 <template>
   <div >
-    <slot></slot>
+    <slot name="home"></slot>
+    <slot name="users"></slot>
   </div>
 </template>
 
---
+++ src/views/Users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="users">
+    <header-text>
+      <template v-slot:users>
+        <h2>users</h2>
+        <div>ユーザ一覧</div>
+      </template>
+    </header-text>
+  </div>
+</template>
+
+<script>
+import HeaderText from '../components/HeaderText.vue'
+
+export default {
+  name: 'users',
+  components: {
+    HeaderText,
+  },
+  data() {
+    return {
+      headerText: "users",
+    }
+  }
+}
+</script>
